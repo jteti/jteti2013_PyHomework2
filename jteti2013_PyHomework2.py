fbb3e64c7d4e5ee35947e19be93daca3e6d7c9b0
jteti <mr.jteti@gmail.com>
2016-04-05 12:31:54 -0400

    Up to Question 5 completed

--- jteti2013_PyHomework2.py
+++ jteti2013_PyHomework2.py
@@ -13,3 +13,31 @@ arr = np.genfromtxt('show_results.txt', dtype='str', delimiter=',')
 # 4. Print the raw data
 print(arr)
 
+# 5. Take the data from the NumPy array and sort it by state, show and
+# viewers, putting each into the appropriate lists you defined earlier.
+# (so now you have 3 lists, one with states, one with shows and one
+# with viewer counts.) No duplicates
+
+# populate states list
+st = 0
+while st < len(arr):
+    states.append(arr[st][0])
+    #convert list to set to eleiminate duplicates, then save back to list\
+    states = list(set(states))
+    st += 1
+
+# populate shows list
+sh = 0
+while sh < len(arr):
+    shows.append(arr[sh][1])
+    #convert list to set to eleiminate duplicates, then save back to list\
+    shows = list(set(shows))
+    sh += 1
+
+# populate viewers list
+v = 0
+while v < len(arr):
+    viewers.append(arr[v][2])
+    v += 1
+
+
