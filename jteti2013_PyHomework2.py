7393697d4b37059686a5137a23a9b32094cb40b6
jteti <mr.jteti@gmail.com>
2016-04-05 12:16:10 -0400

    Up to Question 2 completed

--- jteti2013_PyHomework2.py
+++ jteti2013_PyHomework2.py
@@ -1,1 +1,11 @@
-#1.
+# 1. Move the show_results.txt file from Blackboard into your project directory.
+# File show_results.txt downloaded from blackboard and saved to project directory
+
+# 2. Create 3 lists, one for states, one for shows, and one for viewers
+states = []
+shows = []
+viewers = []
+
+# 3. Injest data from text file and put it into a NumPy array (show_results.txt)
+
+
